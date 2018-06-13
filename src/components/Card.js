adb5aac9b00593e00537f4ffa2dd8993834cb062
Dominic Gan <dominic.gan@mirumagency.com>
2018-06-13 18:05:22 +0800

    fix(card): update markup for card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,9 +4,9 @@ import '../sass/Card.css';
 const Card = (props) => {
 	return (
 		<div className="card">
-			<div className="card__id">{props.id}</div>
+			<div className="card__id">ID: {props.id}</div>
 			<div className="card__avatar">
-				<img width='100' height='100' src={props.avatar} alt={props.first_name + ' ' + props.last_name} />
+				<img src={props.avatar} alt={props.first_name + ' ' + props.last_name} />
 			</div>
 			<div className="card__person">
 			<span className="first-name">{props.first_name}</span>
