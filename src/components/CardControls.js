5ff6efdc5043b7334c17c0633cdbb75e831d8c29
Dominic Gan <dominic.gan@mirumagency.com>
2018-06-18 10:53:29 +0800

    feat(card-controls): add styles for card control info

--- src/components/CardControls.js
+++ src/components/CardControls.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import '../sass/CardControls.css';
 
 const CardControls = (props) => {
 	return (
 		<div className="card-controls">
 			<div className="results">
-			<span className="total">{props.total} result(s)</span><br/>
-			<span className="pages">Page {props.page} of {props.totalPages} page(s)</span><br/>
-			<span className="per-page">{props.perPage} result(s) per page</span>
+			<div className="row results__summary">
+				<span className="total">{props.total} result(s)</span>
+				<span className="per-page">{props.perPage} result(s) per page</span>
+			</div>
+			<div className="row results__pages">
+				<span className="pages">Page {props.page} of {props.totalPages}</span><br/>
+			</div>
 			</div>
 			<div className="pagination">
 				<button className="pagination__prev" onClick={props.handleButtonClick.bind(this, 'prev')} disabled={props.page === 1}>Prev</button>
-				<input type="text" className="pagination__value" value={props.page} onChange={props.handleInputChange}/>
+				<input type="number" className="pagination__value" value={props.page} onChange={props.handleInputChange}/>
 				<button className="pagination__next" onClick={props.handleButtonClick.bind(this, 'next')} disabled={props.page === props.totalPages}>Next</button>
 			</div>
 			<span className={'error ' + (props.error.length ? 'has-error' : '')}>{props.error}</span>
